15ccce725b1907fa83d0bbaad4ca617f833fa1db
Briter Gonzalez <btg.developers@gmail.com>
2023-11-19 23:39:31 +0100

    feat(mvc): add angular js to the views

---
+++ public/javascripts/index.js
@@ -0,0 +1,4 @@
+angular.module('index-module', [])
+  .controller('IndexController', function($scope) {
+    $scope.name = ""
+  });
---
+++ public/javascripts/main.js
@@ -0,0 +1,4 @@
+angular.module('app', ['index-module'])
+  .controller('MainController', function($scope) {
+    $scope.title = "Main Controller"
+  });
